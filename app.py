736d0894b929260e89cedbd58e31de56f6bc625d
Washington Santos <washingtonstk@gmail.com>
2016-01-21 10:36:35 -0300

    Melhoramento da avaliacao

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
-from flask import Flask, request, render_template, session
+from flask import Flask, request, render_template, session, url_for, redirect
 from random import randint
 
 app = Flask(__name__)
 
 app.secret_key = 'ailovebeico'
-app.jinja_env.add_extension('pyjade.ext.jinja.PyJadeExtension')
 
 @app.route("/")
 def index():
@@ -20,20 +19,24 @@ def question():
 	c = randint(session['first'], session['seccond'])
 	d = randint(session['first'], session['seccond'])
 	session['resp']=a*b+c-d
+	session['valid']=0
 	return render_template("question.html", num1=a, num2=b, num3=c, num4=d, acertos=session['count'])
 
 @app.route("/solve/teste", methods=['POST', 'GET'])
 def teste():
 	aluno = int(request.form['resposta'])
-	if session['count'] > 3:
+	if session['count']%5==0:
 		session['first']+=10
 		session['seccond']+=30
 
 	if aluno == session['resp']:
-		session['count']+=1
+		if session['valid']==0:
+			session['count']+=1
+			session['valid']=1
 		return 'ok<br /> <a href="/solve">Continuar</a>'
 	else:
 		session['count']=0
 		return 'errou <br /> <a href="/"> Reiniciar</a>'
 
-app.run(use_reloader=True, host='0.0.0.0')
+if __name__ == '__main__':
+	app.run(use_reloader=True, host='0.0.0.0')
